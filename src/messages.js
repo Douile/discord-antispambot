e8df82ca4f7cb14bc6426896d92f2f32d1c18cdb
Tom <25043847+Douile@users.noreply.github.com>
2019-12-17 21:32:37 +0000

    Add search help message

--- src/messages.js
+++ src/messages.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const HELP_MESSAGES = {
-  'new': 'Create a new spamban rule `new [-time TIME] {RULE}`\nTIME - an optional argument specifying the time restriction for when accounts where created\nRULE - A regex rule for the name or nickname of accounts (for help see <https://regexr.com>)',
+  'new': 'Create a new spamban rule `new [-time TIME] [-length LENGTH] {RULE}`\nTIME - an optional argument specifying the time restriction for when accounts where created\nLENGTH - an optional argument specifying how long in hours the rule will be active\nRULE - A regex rule for the name or nickname of accounts (for help see <https://regexr.com>)',
   'delete': 'Delete a rule `delete {RULEID}`\nRULEID - The ID outputted by the list command (time rule was created in unix)',
+  'search': 'Search all rules that haven\' been deleted `search {TERM}...`\nTERM - Either a rule ID or user (via mention)',
   'list': 'List the currently active rules',
-  'help': 'Print a help message, specify a command using `help {COMMAND}`'
+  'help': 'Print help messages for all commands specified `help {COMMAND}...`\nCOMMAND - exact name of subcommand\nDon\'t specify any commands if you wish to list all commands'
 }
 
 exports.getHelpMessage = function(command) {
