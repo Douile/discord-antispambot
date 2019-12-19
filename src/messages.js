234c3c0ced579131d8feddb14659716f6120d2b8
Tom <25043847+Douile@users.noreply.github.com>
2019-12-19 21:19:35 +0000

    Update help messages

--- src/messages.js
+++ src/messages.js
@@ -2,10 +2,11 @@
 
 const HELP_MESSAGES = {
   'new': 'Create a new spamban rule `new [-time TIME] [-length LENGTH] {RULE}`\nTIME - an optional argument specifying the time restriction for when accounts where created\nLENGTH - an optional argument specifying how long in hours the rule will be active\nRULE - A regex rule for the name or nickname of accounts (for help see <https://regexr.com>)',
-  'delete': 'Delete a rule `delete {RULEID}`\nRULEID - The ID outputted by the list command (time rule was created in unix)',
+  'delete': 'Delete a rule `delete {RULEID}...`\nRULEID - The ID outputted by the list command (time rule was created in unix)',
   'search': 'Search all rules that haven\' been deleted `search {TERM}...`\nTERM - Either a rule ID or user (via mention)',
   'list': 'List the currently active rules',
-  'help': 'Print help messages for all commands specified `help {COMMAND}...`\nCOMMAND - exact name of subcommand\nDon\'t specify any commands if you wish to list all commands'
+  'help': 'Print help messages for all commands specified `help [COMMAND]...`\nCOMMAND - exact name of subcommand\nDon\'t specify any commands if you wish to list all commands',
+  'info': 'Return info about state of the bot'
 }
 
 exports.getHelpMessage = function(command) {
